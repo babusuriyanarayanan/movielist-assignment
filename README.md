fd0a81b15cf347ac945b995c4123d862c9130499
unknown <babusuriyanarayanan@gmail.com>
2017-03-11 15:30:05 +0530

    basic setup - node webpack and react

---
+++ README.md
@@ -0,0 +1,15 @@
+# babu-marlabs-assignment in bitbucket
+
+Assignment
+
+### Instructions
+download and install node js from https://nodejs.org/en/download/
+### npm install -g webpack
+
+```bash
+npm install -g webpack
+npm install
+
+webpack -w
+node server.js
+```
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var engines = require('consolidate');
+var app = express();
+var mustache = require('mustache');
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+   res.render('index.html');
+});
+
+var server = app.listen(3000, function () {
+  console.log('Server running at http://localhost:' + server.address().port)
+});
