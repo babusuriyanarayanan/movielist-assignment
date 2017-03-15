48a5cb102e278fef4efb417afa96db95211a5b6d
unknown <babusuriyanarayanan@gmail.com>
2017-03-15 15:38:57 +0530

    with router and formatted code

--- app/components/datatable/TableItem.js
+++ app/components/datatable/TableItem.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Moment from 'moment';
 
 export const TableItem = (props) => {
 
+  var cDate = Moment(props.item.created_at).format('MMMM Do YYYY, h:mm:ss a')
+  var uDate = Moment(props.item.updated_at).format('MMMM Do YYYY, h:mm:ss a')
+  
     return (
 
-        < ul className = { `col-md-12 ${props.item.color}` } >
+        < ul className = { `col-xs-12 col-md-12 ${props.item.color}` } >
 
-        < li className = "col-md-2" > { props.item.title } < /li> 
-        < li className = "col-md-2" > { props.item.status } < /li> 
-        < li className = "col-md-3" > { props.item.created_at } < /li> 
-        < li className = "col-md-3" > { props.item.updated_at } < /li> 
-        < li className = "col-md-2" > < a href = "#"
+        < li className = "col-xs-12 col-md-2" > { props.item.title } < /li> 
+        < li className = "col-xs-12 col-md-2" > { props.item.status } < /li> 
+        < li className = "col-xs-12 col-md-3" > { cDate } < /li> 
+        < li className = "col-xs-12 col-md-3" > { uDate } < /li> 
+        < li className = "col-xs-12 col-md-2" > < a href = "#"
         onClick = {
             () => props.handleRemove(props.item.id) } > Delete < /a></li >
         < /ul> 
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Main from '../components/Main';
-import Home from '../components/Home';
-import Profile from '../components/Profile';
-import {Route, IndexRoute} from 'react-router';
+import App from '../App';
+import Service from '../components/service/listservice';
+import { Route } from 'react-router'
 
 export default (
-  <Route path="/" component={Main}> 
-  <Route path="/profile" component={Profile}> </Route>
-  <Route path="/home" component={Home}></Route>
-  </Route>
+  <div>
+    <Route path="/" component={App}/>
+    <Route path="/data" component={Service}/>
+  </div>
 
 );
